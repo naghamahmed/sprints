619e5453d1bcf406b498b5dfbb85ed30a597256a
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-01-08 07:40:51 +0200

    Create task1.py

---
+++ Python/task1.py
@@ -0,0 +1,13 @@
+#Replaceing character in the giving string with givin index by split the string into two parts
+#the first part "str[:index]" the characters from thr first till the new character(without the new char) then "+" the new character
+#the second part "str[index+1:]" the rest string after the new character till the end
+#Calling the function
+
+def change_char(str, index, char):
+    return str[:index] + char + str[index+1:]
+
+
+print(change_char("Abracadabra",5, "K"))  # return Abrackdabra
+
+
+
