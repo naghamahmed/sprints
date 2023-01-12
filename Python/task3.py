6d1f42e8f3ac90f48242cc9c0fb19022e42ec136
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-01-12 07:28:45 +0200

    Balanced Parentheses

---
+++ Python/task3.py
@@ -0,0 +1,24 @@
+#while loop will check the sequence of the string and replace each sequence with empty string untill the string becomes empty
+#If there is no pair present, then returns the True otherwise False
+
+def isBalanced(string):
+    while True:
+        if '()' in string:
+            string = string.replace('()', '')
+        elif '{}' in string:
+            string = string.replace('{}', '')
+        elif '[]' in string:
+            string = string.replace('[]', '')
+        else:
+            return "YES" if not string else "NO"
+
+
+# Test code
+string = "{[()]}"
+print(isBalanced(string)) #retrun YES
+
+string = "{[(]}"
+print(isBalanced(string)) #return NO
+
+string = "{{[[(())]]}}"
+print(isBalanced(string)) #return YES
